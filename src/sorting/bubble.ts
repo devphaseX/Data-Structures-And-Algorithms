e0209f88adb02adbcfcfe059c3ed25561cb92736
devphasex <wilsonace87@gmail.com>
2022-01-21 17:25:46 +0100

    enhanced my bubble sort algorithm to avoid unnecessary pass checking.

--- src/sorting/bubble.ts
+++ src/sorting/bubble.ts
@@ -4,27 +4,29 @@ import { SortPredicateFn } from '../util/type';
 function sortByBubble<T>(list: Array<T>, predicateFn: SortPredicateFn<T>) {
   function _bubbleSort(
     list: Array<T>,
-    isSortedBeforePassEnd = 0,
+    isSortedBeforePassEnd = false,
     order = list.length - 1
   ): Array<T> {
-    if (order < 0 || isSortedBeforePassEnd === 0) return list;
+    if (order < 0 || isSortedBeforePassEnd) return list;
 
+    let swapFlag = 0;
     rangeLoop(0, order, (i, j) => {
       const currentItemEntry = createItemEntry(list[i], i);
       const nextItemEntry = createItemEntry(list[j], j);
 
       if (predicateFn(currentItemEntry, nextItemEntry)) {
         swapItem(list, currentItemEntry, nextItemEntry);
+        swapFlag = 1;
       }
     });
 
-    return _bubbleSort(list, isSortedBeforePassEnd, order - 1);
+    return _bubbleSort(list, swapFlag === 0, order - 1);
   }
 
   function _bubbleSortOptimised(list: Array<T>) {
     const size = list.length;
     rangeLoop(0, size - 1, (pass, _, breakLoop) => {
-      let isSortedBeforePassEnd = 0;
+      let swapFlag = 0;
 
       rangeLoop(0, size - 1 - pass, (i, j) => {
         const currentItemEntry = createItemEntry(list[i], i);
@@ -32,11 +34,11 @@ function sortByBubble<T>(list: Array<T>, predicateFn: SortPredicateFn<T>) {
 
         if (predicateFn(currentItemEntry, nextItemEntry)) {
           swapItem(list, currentItemEntry, nextItemEntry);
-          isSortedBeforePassEnd = 1;
+          swapFlag = 1;
         }
       });
 
-      if (isSortedBeforePassEnd === 0) {
+      if (swapFlag === 0) {
         breakLoop();
       }
     });
