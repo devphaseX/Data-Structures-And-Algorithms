056d709174d20e1952b3884138b0656af6574c31
devphasex <wilsonace87@gmail.com>
2022-09-12 11:26:48 +0100

    implement a levl order traversal

---
+++ src/data_structure/tree/traversal/levelorder.ts
@@ -0,0 +1,22 @@
+import createQueue from '../../queue/index';
+import { unwrapTreeValue } from '../shared';
+import type { BinaryTree, TreeTraversalFn } from '../shared.types';
+
+const levelorderTraversal = <T>(
+  rootTree: BinaryTree<T>,
+  cb: TreeTraversalFn<T>
+) => {
+  const rootMantainedQueue = createQueue<BinaryTree<T>>(null);
+  let currentRoot = rootTree as BinaryTree<T> | null;
+  rootMantainedQueue.enqueue(rootTree);
+
+  while (!rootMantainedQueue.isEmpty()) {
+    currentRoot = rootMantainedQueue.dequeue()!;
+    cb(unwrapTreeValue(currentRoot));
+
+    if (currentRoot.left) rootMantainedQueue.enqueue(currentRoot.left);
+    if (currentRoot.right) rootMantainedQueue.enqueue(currentRoot.right);
+  }
+};
+
+export default levelorderTraversal;
