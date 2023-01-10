4b0d02b5a39f5e2b38cf2e8bcd079ec1f4b236bd
devphasex <wilsonace87@gmail.com>
2023-01-10 12:48:48 +0100

    implement find number of internal nodes algorithm

---
+++ src/practicalProblems/tree/findInternalNodes.ts
@@ -0,0 +1,30 @@
+import { BinaryTree } from '../../data_structure/tree/shared.types';
+import createQueue from '../../data_structure/queue';
+
+function findInternalNodes<T>(tree: BinaryTree<T>) {
+  const queue = createQueue<NonNullable<typeof tree> | null>(null);
+  queue.enqueue(tree);
+  queue.enqueue(null);
+  const leaves: Array<typeof tree> = [];
+
+  while (!queue.isEmpty()) {
+    let temp = queue.dequeue();
+    if (temp && temp.left && temp.right) leaves.push(temp);
+    if (temp === null) {
+      queue.dequeue();
+      if (!queue.isEmpty()) queue.enqueue(null);
+    } else {
+      if (temp.left) {
+        queue.enqueue(temp.left);
+      }
+
+      if (temp.right) {
+        queue.enqueue(temp.right);
+      }
+    }
+  }
+
+  return leaves;
+}
+
+export { findInternalNodes };
