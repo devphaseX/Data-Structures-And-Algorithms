f1ec5e79d92dc3bf76cde7d089528fa6de57d9ad
devphasex <wilsonace87@gmail.com>
2023-01-10 13:21:17 +0100

    implement a diameter algorithm

---
+++ src/practicalProblems/tree/diameterOfTree.ts
@@ -0,0 +1,19 @@
+import { BinaryTree } from '../../data_structure/tree/shared.types';
+
+function getTreeDiameter(tree: BinaryTree<any> | null | undefined) {
+  function _getTreeDiamter(_tree: typeof tree, diameter = { count: 0 }) {
+    if (!tree) return 0;
+    const left = _getTreeDiamter(_tree, diameter);
+    const right = _getTreeDiamter(_tree, diameter);
+
+    if (left + right > diameter.count) {
+      diameter.count = left + right;
+    }
+
+    return Math.max(left, right) + 1;
+  }
+
+  return _getTreeDiamter(tree);
+}
+
+export { getTreeDiameter };
